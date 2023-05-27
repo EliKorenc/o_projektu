4d174ffb780ee624a12045dd049204c6a5ec9081
Veronika Bendova <svechovaver@gmail.com>
2023-05-28 01:11:07 +0200

    Přidání loga githubu

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-console.log('Ahoj planetko!');
+const navLinks = document.querySelectorAll('.nav-link');
+
+navLinks.forEach(function (navLink) {
+  navLink.addEventListener('click', function () {
+    var navbarCollapse = document.querySelector('.navbar-collapse');
+    navbarCollapse.classList.remove('show');
+  });
+});
