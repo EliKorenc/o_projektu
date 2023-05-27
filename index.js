b9ff8175e09333bf156c88912f61cf8be0056392
Veronika Bendova <svechovaver@gmail.com>
2023-05-28 00:45:20 +0200

    Oprava v navigační liště

--- index.js
+++ index.js
@@ -1,11 +1,1 @@
 console.log('Ahoj planetko!');
-
-const planetka = document.querySelector('.planetka_strach');
-
-planetka.addEventListener('mouseover', () => {
-  planetka.classList.add('shaking');
-});
-
-planetka.addEventListener('mouseout', () => {
-  planetka.classList.remove('shaking');
-});
