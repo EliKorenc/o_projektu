b4b1b3daddd3c10631889837f68b3de80d7b28ce
Eliška Korencová <eli.korenc@gmail.com>
2023-05-29 00:04:12 +0200

    Přidání patičky pommocí js, stylování stránek pro její vykreslení.

--- index.js
+++ index.js
@@ -4,3 +4,18 @@ const menu = document.querySelector('.menu');
 hamburgerMenu.addEventListener('click', function () {
   menu.classList.toggle('open');
 });
+
+const footerElm = document.getElementById('footer');
+footerElm.innerHTML = `
+  <a
+  class="footer__odkaz--akademie"
+  href="https://www.czechitas.cz/kurzy/digitalni-akademie-web"
+  target="_blank"
+  >DA WEB, 2023</a
+>
+<div class="footer__odkaz--czechitas">
+  <a href="https://www.czechitas.cz/" target="_blank"
+    ><img src="./img/logo_czechitas.svg" alt="logo Czechitas z.ú."
+  /></a>
+</div>
+`;
